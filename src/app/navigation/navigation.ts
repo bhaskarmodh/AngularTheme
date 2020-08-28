dfdd0d619093f68928747c941faf0fe6879283ce
Jaydip Gambhava <jaydip@galitein.co.in>
2020-08-28 18:19:09 +0530

    Form Design

--- src/app/navigation/navigation.ts
+++ src/app/navigation/navigation.ts
@@ -14,20 +14,7 @@ export const navigation: FuseNavigation[] = [
                 translate: 'NAV.DASHBOARDS',
                 type     : 'collapsable',
                 icon     : 'dashboard',
-                children : [
-                    {
-                        id   : 'analytics',
-                        title: 'Analytics',
-                        type : 'item',
-                        url  : '/apps/dashboards/analytics'
-                    },
-                    {
-                        id   : 'project',
-                        title: 'Project',
-                        type : 'item',
-                        url  : '/apps/dashboards/project'
-                    }
-                ]
+                url      : '/apps/dashboard'
             },
             {
                 id       : 'calendar',
